24613c51fd9bc68a7e074454c44e47b05c1ae075
Cristina369 <93976122+Cristina369@users.noreply.github.com>
2021-11-09 13:05:25 +0200

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,12 +1,12 @@
-var video = document.getElementById("video");//-----------------------------------------------
+let video = document.getElementById("video");//-----------------------------------------------
 function on_cam_success(stream){video.srcObject = stream;}
 //-----------------------------------------------
 function on_cam_error(err){alert("error."+err.message);}
 //-----------------------------------------------
-var constraints = { audio: false, video: true };
+let constraints = { audio: false, video: true };
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam_success).catch(on_cam_error);
 //-----------------------------------------------
-function capteaza(){var c = document.getElementById("canvas");
+function capteaza(){let c = document.getElementById("canvas");
 c.width = video.width;c.height = video.height;
-var ctx = c.getContext("2d");ctx.drawImage(video, 0, 0, 640, 480);
+let ctx = c.getContext("2d");ctx.drawImage(video, 0, 0, 640, 480);
 }
